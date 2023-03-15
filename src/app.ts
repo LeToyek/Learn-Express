9ff44b8ffec2f6e79a142f83394c04c6a298e45c
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-03-15 10:42:24 +0700

    [INIT] nodemon

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,33 @@
 import { config } from "dotenv";
+
 import express, { Application, NextFunction, Request, Response } from 'express';
+import cats from "./model/cat";
+
 
 config()
 
 const app: Application =express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+
+
 app.get('/',(req: Request,res :Response, next: NextFunction)=>{
   res.send("Route Get Work")
 })
+app.get('/test',(req: Request,res :Response, next: NextFunction)=>{
+  res.send("Testt")
+})
+app.post('/test',(req: Request, res :Response)=>{ 
+  cats.push(req.body)
+  console.log(cats.map((c) => c.name))
+  return res.status(201).send("Cat has been Created "+ cats.map(c => {c.age}))
+})
+
+// app.route("/car").get(
+
+// )
 
 const PORT = process.env.PORT || 3000;
 
---
+++ src/model/cat.ts
@@ -0,0 +1,12 @@
+type Cat = {
+  name: string,
+  age: number
+}
+
+const cats : Cat[]= [
+  {name: "asdas",age: 3},
+  {name: "joko", age: 4}
+] 
+
+export default cats
+
